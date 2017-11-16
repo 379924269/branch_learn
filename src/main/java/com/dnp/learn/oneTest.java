cdc0a64f02019057e9a6bb4341302bd8e5bd9a96
huazai <379924269@qq.com>
2017-11-16 11:18:28 +0800

    我想用apollo推送

---
+++ src/main/java/com/dnp/learn/oneTest.java
@@ -0,0 +1,12 @@
+package com.dnp.learn;
+
+/**
+ * @Author 华仔
+ * @Author 2017/11/16 11:15
+ */
+public class oneTest {
+
+    public void test1() {
+        String msg = "test1版本我想用的信息用apoll推送";
+    }
+}
