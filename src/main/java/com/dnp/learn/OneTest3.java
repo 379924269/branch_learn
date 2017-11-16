fd68d6f72608ba5d9f3b445febf48986d7abb9a0
huazai <379924269@qq.com>
2017-11-16 11:26:33 +0800

    在apollo推送上继续开发

---
+++ src/main/java/com/dnp/learn/OneTest3.java
@@ -0,0 +1,11 @@
+package com.dnp.learn;
+
+/**
+ * @Author 华仔
+ * @Author 2017/11/16 11:22
+ */
+public class OneTest3 {
+    public void test1() {
+        String msg = "我继续在apollo推送上继续开发";
+    }
+}
