298f23929b29b8185f77dd368d014f4952507d5a
huazai <379924269@qq.com>
2017-11-16 14:52:35 +0800

    MX分支

---
+++ src/main/java/com/dnp/learn/MxClass.java
@@ -0,0 +1,12 @@
+package com.dnp.learn;
+
+/**
+ * @Author 华仔
+ * @Author 2017/11/16 14:51
+ */
+public class MxClass
+{
+    public void say() {
+        System.out.println("======分支MX上的东西=======");
+    }
+}
