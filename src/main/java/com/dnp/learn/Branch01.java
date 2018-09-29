703dec07388fc2f11b445dc60fdfe00067b3caae
huazai <379924269@qq.com>
2018-09-29 09:02:30 +0800

    主干继续开发

---
+++ src/main/java/com/dnp/learn/Branch01.java
@@ -0,0 +1,16 @@
+package com.dnp.learn;
+
+/**
+ * @author 华仔
+ * @date 2018/9/29 9:01
+ */
+public class Branch01 {
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
